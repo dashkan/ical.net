5761f705a7b957a511ced14d2e9ac9c6d2de0dcf
Ashkan Daie <ashkan.daie@gmail.com>
2023-02-22 11:04:39 -0800

    Updates

--- src/Ical.Net/Serialization/GenericListSerializer.cs
+++ src/Ical.Net/Serialization/GenericListSerializer.cs
@@ -2,77 +2,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 
-namespace Ical.Net.Serialization;
-
-public class GenericListSerializer : SerializerBase
+namespace Ical.Net.Serialization
 {
-    private readonly Type _innerType;
-    private readonly Type _objectType;
-
-    public GenericListSerializer(Type objectType)
+    public class GenericListSerializer : SerializerBase
     {
-        _innerType = objectType.GetGenericArguments()[0];
+        private readonly Type _innerType;
+        private readonly Type _objectType;
 
-        var listDef = typeof (List<>);
-        _objectType = listDef.MakeGenericType(typeof (object));
-    }
-
-    public override Type TargetType => _objectType;
-
-    public override string SerializeToString(object obj) => throw new NotImplementedException();
-
-    private MethodInfo _addMethodInfo;
-    public override object Deserialize(TextReader tr)
-    {
-        var p = SerializationContext.Peek() as ICalendarProperty;
-        if (p == null)
+        public GenericListSerializer(Type objectType)
         {
-            return null;
+            _innerType = objectType.GetGenericArguments()[0];
+
+            var listDef = typeof (List<>);
+            _objectType = listDef.MakeGenericType(typeof (object));
         }
 
-        // Get a serializer factory to deserialize the contents of this list
-        var listObj = Activator.CreateInstance(_objectType);
-        if (listObj == null)
-        {
-            return null;
-        }
+        public override Type TargetType => _objectType;
 
-        // Get a serializer for the inner type
-        var sf = GetService<ISerializerFactory>();
-        var stringSerializer = sf.Build(_innerType, SerializationContext) as IStringSerializer;
-        if (stringSerializer == null)
-        {
-            return null;
-        }
-        // Deserialize the inner object
-        var value = tr.ReadToEnd();
+        public override string SerializeToString(object obj) => throw new NotImplementedException();
 
-        // If deserialization failed, pass the string value into the list.
-        var objToAdd = stringSerializer.Deserialize(new StringReader(value)) ?? value;
-
-        // TODO: cache this
-        if (_addMethodInfo == null)
+        public override object Deserialize(TextReader tr)
         {
-            _addMethodInfo = _objectType.GetMethod("Add")!;
-        }
-
-        // Determine if the returned object is an IList<ObjectType>, rather than just an ObjectType.
-        if (objToAdd is IList add)
-        {
-            //Deserialization returned an IList<ObjectType>, instead of an ObjectType.  So enumerate through the items in the list and add
-            //them individually to our list.
-            foreach (var innerObj in add)
+            var p = SerializationContext.Peek() as ICalendarProperty;
+            if (p == null)
             {
-                _addMethodInfo.Invoke(listObj, new[] {innerObj});
+                return null;
             }
+
+            // Get a serializer factory to deserialize the contents of this list
+            var listObj = (IList)Activator.CreateInstance(_objectType);
+            if (listObj == null)
+            {
+                return null;
+            }
+
+            // Get a serializer for the inner type
+            var sf = GetService<ISerializerFactory>();
+            var stringSerializer = sf.Build(_innerType, SerializationContext) as IStringSerializer;
+            if (stringSerializer == null)
+            {
+                return null;
+            }
+            // Deserialize the inner object
+            var value = tr.ReadToEnd();
+
+            // If deserialization failed, pass the string value into the list.
+            var objToAdd = stringSerializer.Deserialize(new StringReader(value)) ?? value;
+
+            // Determine if the returned object is an IList<ObjectType>, rather than just an ObjectType.
+            if (objToAdd is IList add)
+            {
+                //Deserialization returned an IList<ObjectType>, instead of an ObjectType.  So enumerate through the items in the list and add
+                //them individually to our list.
+                foreach (var innerObj in add)
+                {
+                    listObj.Add(new[] {innerObj});
+                }
+            }
+            else
+            {
+                // Add the object to the list
+                listObj.Add(new[] {objToAdd});
+            }
+            return listObj;
         }
-        else
-        {
-            // Add the object to the list
-            _addMethodInfo.Invoke(listObj, new[] {objToAdd});
-        }
-        return listObj;
     }
 }
